af22cef2fdcbf559eae17894c4ebdbcf5218b055
dicaristic <dimitrije.ristic2112@outlook.com>
2025-05-03 22:49:06 +0200

    training loop progress

--- dataloaders/playpen_dataloader.py
+++ dataloaders/playpen_dataloader.py
@@ -2,3 +2,30 @@
 Implement a dataloader to load batches 
 
 """
+
+from torch.utils.data import Dataset
+import torch
+
+class StepRolloutDataloader(Dataset):
+    def __init__(self, trajectories):
+        self.samples = []
+        # Flatten trajectories into individual transition samples
+        for trajectory in trajectories:
+            for i in range(len(trajectory) - 1):  # -1 to handle next_obs
+                current = trajectory[i]
+                next_step = trajectory[i + 1]
+                
+                self.samples.append({
+                    'obs': current['context'],
+                    'action': current['response'],
+                    'reward': current['info'].get('reward', 0),
+                    'next_obs': next_step['context'],
+                    'done': current['done']
+                })
+    
+    def __len__(self):
+        return len(self.samples)
+    
+    def __getitem__(self, idx):
+        sample = self.samples[idx]
+        return sample
